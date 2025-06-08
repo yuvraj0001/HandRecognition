d91249e621ccb740a7453d0774a925e55e4aae6e
Yuvraj Kumar <yuvi4867@gmail.com>
2025-06-08 14:11:52 +0530

    created landmark and hand connections

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -2,10 +2,30 @@ import cv2
 import mediapipe as mp
 import time
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+ptime = 0
+
 cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = hands.process(imgRGB)
+    # print(result.multi_hand_landmarks)
 
+    if result.multi_hand_landmarks:
+        for handLms in result.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+
+    ctime = time.time()
+    fps = 1/(ctime-ptime)
+    ptime = ctime
+    print(fps)
+
+    cv2.putText(img, f'FPS : {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
